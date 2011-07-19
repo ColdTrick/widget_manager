ab0b542a19f9852960caccdac38e44f16d0f7273
Jerome Bakker <jeabakker@coldtrick.com>
2011-07-19 10:41:08 +0200

    removed: fancybox old css file loading

--- views/default/widget_manager/metatags.php
+++ views/default/widget_manager/metatags.php
@@ -28,7 +28,6 @@
 		if(empty($fancybox_js_loaded)){
 			$fancybox_js_loaded = true;
 	?>
-	<link rel="stylesheet" href="<?php echo $vars["url"];?>mod/widget_manager/vendors/fancybox/jquery.fancybox-1.3.4.css" type="text/css" />
 	<script type="text/javascript" src="<?php echo $vars["url"];?>mod/widget_manager/vendors/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
 	
 	<?php }?>
