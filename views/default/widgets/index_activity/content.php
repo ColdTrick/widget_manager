9100e0c54044dff698ecb0e15b2817619a336cf0
jdalsem <jdalsem@coldtrick.com>
2012-05-10 09:58:43 +0200

    restored index_activity widget functionality

--- views/default/widgets/index_activity/content.php
+++ views/default/widgets/index_activity/content.php
@@ -1,6 +1,4 @@
 <?php 
-	global $CONFIG;
-	
 	$widget = $vars["entity"];
 	
 	$count = sanitise_int($widget->activity_count, false);
@@ -8,56 +6,35 @@
 		$count = 10;
 	}
 	
-	//$activity_content = $widget->getMetadata("activity_content");
+	$activity_content = $widget->activity_content;
 	
 	$river_options = array(
 			"pagination" => false,
-			"limit" => $count
+			"limit" => $count,
+			"type_subtype_pairs" => array()
 		);
 	
 	if(empty($activity_content)){
 		$activity = elgg_list_river($river_options);
-		//$activity = elgg_view_river_items(0, 0, "", "", "", '', $count, 0, 0, FALSE);
 	} else {
-		/*
-		// Construct 'where' clauses for the river
-		$where = array();
-		// river table does not have columns expected by get_access_sql_suffix so we modify its output
-		$where[] = str_replace("and enabled='yes'",'',str_replace('owner_guid','subject_guid',get_access_sql_suffix()));
-		
-		$content_where_filter = array();
 		foreach($activity_content as $content){
 			list($type, $subtype) = explode(",", $content);
 			if(!empty($type)){
-				$content_where = " (type = '{$type}'";
-				if($subtype){
-					$content_where .= " AND subtype = '{$subtype}'";
+				$value = $subtype;
+				if(array_key_exists($type, $river_options['type_subtype_pairs'])){
+					if(!is_array($river_options['type_subtype_pairs'][$type])){
+						$value = array($river_options['type_subtype_pairs'][$type]);
+					} else {
+						$value = $river_options['type_subtype_pairs'][$type];
+					}
+					
+					$value[] = $subtype;
 				}
-				$content_where .= ")";
-				$content_where_filter[] = $content_where;
+				$river_options['type_subtype_pairs'][$type] = $value;
 			}
 		}
-			
-		$where[] = " " . implode(' or ', $content_where_filter);
 		
-		$whereclause = implode(' and ', $where);
-		
-		// Get input from outside world and sanitise it
-		$offset = (int) get_input('offset',0);
-		
-		$sql = "select id,type,subtype,action_type,access_id,view,subject_guid,object_guid,annotation_id,posted" .
-			 		" from {$CONFIG->dbprefix}river where {$whereclause} order by posted desc limit {$offset},{$count}";
-		
-		// Get river items, if they exist
-		if ($riveritems = get_data($sql)) {
-		
-			$activity = elgg_view('river/item/list',array(
-				'limit' => $count,
-				'offset' => $offset,
-				'items' => $riveritems,
-				'pagination' => false
-			));
-		}*/
+		$activity = elgg_list_river($river_options);
 	}
 	
 	if(empty($activity)){
--- views/default/widgets/index_activity/edit.php
+++ views/default/widgets/index_activity/edit.php
@@ -1,44 +1,43 @@
 <?php 
+
+	$widget = $vars["entity"];
 	
-	$count = sanitise_int($vars["entity"]->activity_count, false);
+	$count = sanitise_int($widget->activity_count, false);
 	if(empty($count)){
 		$count = 10;
 	}
 	
-	/*$contents = array();
+	$contents = array();
 	
-	if (!empty($vars['config']->registered_entities)) {
-		foreach ($vars['config']->registered_entities as $type => $ar) {
-			if (count($vars['config']->registered_entities[$type])) {
-				foreach ($vars['config']->registered_entities[$type] as $subtype) {
+	$registered_entities = elgg_get_config("registered_entities");
+	
+	if (!empty($registered_entities)) {
+		foreach ($registered_entities as $type => $ar) {
+			if (count($registered_entities[$type])) {
+				foreach ($registered_entities[$type] as $subtype) {
 					$keyname = 'item:' . $type . ':' . $subtype;
-					$contents[$keyname] = "{$type},{$subtype}";
+					$contents[elgg_echo($keyname)] = "{$type},{$subtype}";
 				}
 			} else {
 				$keyname = 'item:' . $type;
-				$contents[$keyname] = "{$type},";
+				$contents[elgg_echo($keyname)] = "{$type},";
 			}
 		}
-	}*/
+	}
 
 ?>
 <div>
-	<?php echo elgg_echo("widget:numbertodisplay"); ?><br />
-	<?php echo elgg_view("input/text", array("name" => "params[activity_count]", "value" => $count, "size" => "4", "maxlength" => "4"));?>
-</div>
-<?php /*?>
-<div>
-	<?php echo elgg_echo("filter"); ?><br />
 	<?php 
-	$activity_content = $vars["entity"]->getMetadata("activity_content");
-	
-	echo elgg_view("input/hidden", array("name" => "params[activity_content][]", "value" => "")); // needed to be able to store no selection
-	foreach($contents as $label => $content){
-		if(in_array($content, $activity_content)){
-			echo "<input type='checkbox' name='params[activity_content][]' checked='checked' value='" . $content . "'>" . elgg_echo($label) . "<br />";
-		} else {
-			echo "<input type='checkbox' name='params[activity_content][]' value='" . $content . "'>" . elgg_echo($label) . "<br />";
-		}
-	}
+		echo elgg_echo("widget:numbertodisplay"); 
+		echo elgg_view("input/text", array("name" => "params[activity_count]", "value" => $count, "size" => "4", "maxlength" => "4"));
 	?>
-</div>*/
+</div>
+
+<div>
+	<?php 
+		echo elgg_echo("filter"); 
+		
+		$activity_content = $vars["entity"]->activity_content;
+		echo elgg_view("input/checkboxes", array("name" => "params[activity_content]", "options" => $contents, "value" => $activity_content));
+	?>
+</div>
