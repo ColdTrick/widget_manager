1a79b79c1ddb2434f58056c74a1dff6a51d86d81
jdalsem <jdalsem@coldtrick.com>
2012-05-14 13:35:57 +0200

    changed: freehtml is now contained within a div with elgg-output class for nicer text layout

--- views/default/widgets/free_html/content.php
+++ views/default/widgets/free_html/content.php
@@ -2,7 +2,7 @@
 	$widget = $vars["entity"];
 	
 	if(!empty($widget->html_content)){
-		echo $widget->html_content;
+		echo "<div class='elgg-output'>" . $widget->html_content . "</div>";
 	} else {
 		echo elgg_echo("widgets:free_html:no_content");
 	}
