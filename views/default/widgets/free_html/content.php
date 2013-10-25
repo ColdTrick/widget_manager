50d6c72f1e9ea52873fc5b1cfc881850b09ba152
Matt Beckett <beck24@gmail.com>
2013-10-25 15:42:29 -0700

    allow parsing of urls in free_html widget

--- views/default/widgets/free_html/content.php
+++ views/default/widgets/free_html/content.php
@@ -2,7 +2,7 @@
 	$widget = $vars["entity"];
 	
 	if(!empty($widget->html_content)){
-		echo "<div class='elgg-output'>" . $widget->html_content . "</div>";
+		echo elgg_view('output/longtext',  array('value' => $widget->html_content));
 	} else {
 		echo elgg_echo("widgets:free_html:no_content");
 	}
