8de7fff5263191a323b8040bd282d9e5fd030447
Jeroen Dalsem <jdalsem@coldtrick.com>
2014-03-07 12:33:58 +0100

    cleanup admin js

--- views/default/js/widget_manager/admin.php
+++ views/default/js/widget_manager/admin.php
@@ -1,18 +1,8 @@
 <?php ?>
-function widget_manager_fix_widget(elem, guid) {
-	var url = "<?php echo elgg_add_action_tokens_to_url($vars["url"] . "action/widget_manager/widgets/toggle_fix"); ?>";
-	$.post(url, {
-		guid: guid}, function(data) {
-			if (data) {
-				$(elem).toggleClass("fixed");
-					
-				$(elem).parents("div.collapsable_box").parent().toggleClass("free_widgets");
-				$(elem).parents("div.collapsable_box_header").toggleClass("fixed_widget").toggleClass("draggable_widget");
-			}
-		});
-}
+//<script>
+elgg.provide("elgg.widget_manager");
 
-$(document).ready(function() {
+elgg.widget_manager.init_admin = function() {
 	$(".widget-manager-fix").live("click", function(event) {
 		$(this).toggleClass("fixed");
 		guid = $(this).attr("href").replace("#", "");
@@ -32,4 +22,8 @@ $(document).ready(function() {
 	$("#widget-manager-settings-extra-contexts .elgg-icon-delete").live("click", function(event) {
 		$(this).parent().parent().remove();
 	});
-});
+}
+
+
+//register init hook
+elgg.register_hook_handler("init", "system", elgg.widget_manager.init_admin);
