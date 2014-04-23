31e7070d6206c9ca19c82ef09836b5ab055d0f55
Jeroen Dalsem <jdalsem@coldtrick.com>
2014-04-23 10:56:52 +0200

    free_html widget changed to long text input

--- views/default/widgets/free_html/edit.php
+++ views/default/widgets/free_html/edit.php
@@ -3,5 +3,5 @@ $widget = $vars["entity"];
 ?>
 <div>
 	<?php echo elgg_echo("widgets:free_html:settings:html_content"); ?><br />
-	<?php echo elgg_view("input/plaintext", array("name" => "params[html_content]", "value" => $widget->html_content)); ?>
+	<?php echo elgg_view("input/longtext", array("name" => "params[html_content]", "value" => $widget->html_content)); ?>
 </div>
