86b0c42c0d715892ca0736fb3d85ca35803ca8ed
Jeroen Dalsem <jdalsem@coldtrick.com>
2014-06-04 09:31:56 +0200

    fixed issue where non admin widget moderator could not see widget title if title was hidden

--- views/default/object/widget.php
+++ views/default/object/widget.php
@@ -69,7 +69,7 @@ if ($widget->widget_manager_custom_class) {
 }
 
 if ($widget->widget_manager_hide_header == "yes") {
-	if (elgg_is_admin_logged_in()) {
+	if ($can_edit) {
 		$widget_class .= " widget_manager_hide_header_admin";
 	} else {
 		$widget_class .= " widget_manager_hide_header";
@@ -80,7 +80,7 @@ if ($widget->widget_manager_disable_widget_content_style == "yes") {
 	$widget_class .= " widget_manager_disable_widget_content_style";
 }
 
-if (($widget->widget_manager_hide_header != "yes") || elgg_is_admin_logged_in()) {
+if (($widget->widget_manager_hide_header != "yes") || $can_edit) {
 	$widget_header = <<<HEADER
 		<div class="elgg-widget-handle clearfix"><h3>$title</h3>
 		$controls
