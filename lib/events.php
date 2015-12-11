9b32b3741c277734f20aa446f596720e41f32e40
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-12-11 13:52:08 +0100

    chore: code styling

--- lib/events.php
+++ lib/events.php
@@ -14,11 +14,16 @@
  * @return void
  */
 function widget_manager_create_group_event_handler($event, $object_type, $object) {
-	if ($object instanceof ElggGroup) {
-		if (elgg_get_plugin_setting("group_option_default_enabled", "widget_manager") == "yes") {
-			$object->widget_manager_enable = "yes";
-		}
+	
+	if (!elgg_instanceof($object, 'group')) {
+		return;
 	}
+	
+	if (elgg_get_plugin_setting('group_option_default_enabled', 'widget_manager') !== 'yes') {
+		return;
+	}
+	
+	$object->widget_manager_enable = 'yes';
 }
 
 /**
@@ -31,24 +36,28 @@ function widget_manager_create_group_event_handler($event, $object_type, $object
  * @return void
  */
 function widget_manager_update_widget($event, $object_type, $object) {
-	if (($object instanceof ElggWidget) && in_array($event, array("create", "update", "delete"))) {
-		if (stristr($_SERVER["HTTP_REFERER"], "/admin/appearance/default_widgets")) {
-			// on create set a parent guid
-			if ($event == "create") {
-				$object->fixed_parent_guid = $object->guid;
-			}
-			
-			// update time stamp
-			$context = $object->context;
-			if (empty($context)) {
-				// only situation is on create probably, as context is metadata and saved after creation of the object, this is the fallback
-				$context = get_input("context", false);
-			}
-			
-			if ($context) {
-				elgg_set_plugin_setting($context . "_fixed_ts", time(), "widget_manager");
-			}
-		}
+	if (!($object instanceof ElggWidget) || !in_array($event, ['create', 'update', 'delete'])) {
+		return;
+	}
+
+	if (!stristr($_SERVER['HTTP_REFERER'], '/admin/appearance/default_widgets')) {
+		return;
+	}
+	
+	// on create set a parent guid
+	if ($event == 'create') {
+		$object->fixed_parent_guid = $object->guid;
+	}
+	
+	// update time stamp
+	$context = $object->context;
+	if (empty($context)) {
+		// only situation is on create probably, as context is metadata and saved after creation of the object, this is the fallback
+		$context = get_input('context', false);
+	}
+	
+	if ($context) {
+		elgg_set_plugin_setting($context . '_fixed_ts', time(), 'widget_manager');
 	}
 }
 
@@ -62,30 +71,38 @@ function widget_manager_update_widget($event, $object_type, $object) {
  * @return void
  */
 function widget_manager_create_object_handler($event, $object_type, $object) {
-
-	if (elgg_instanceof($object, "object", "widget", "ElggWidget")) {
-		$owner = $object->getOwnerEntity();
-		// Updates access for privately created widgets in a group or on site
-		if ((int) $object->access_id === ACCESS_PRIVATE) {
-			$old_ia = elgg_set_ignore_access();
-			if ($owner instanceof ElggGroup) {
-				$object->access_id = $owner->group_acl;
-				$object->save();
-			} elseif ($owner instanceof ElggSite) {
-				$object->access_id = ACCESS_PUBLIC;
-				$object->save();
-			}
-			elgg_set_ignore_access($old_ia);
-		}
-		// Adds a relation between a widget and a multidashboard object
-		$dashboard_guid = get_input("multi_dashboard_guid");
-		if ($dashboard_guid && widget_manager_multi_dashboard_enabled()) {
-			$dashboard = get_entity($dashboard_guid);
-			if (elgg_instanceof($dashboard, "object", MultiDashboard::SUBTYPE, "MultiDashboard")) {
-				add_entity_relationship($object->getGUID(), MultiDashboard::WIDGET_RELATIONSHIP, $dashboard->getGUID());
-			}
-		}
+	
+	if (!elgg_instanceof($object, 'object', 'widget', 'ElggWidget')) {
+		return;
 	}
+
+	$owner = $object->getOwnerEntity();
+	
+	// Updates access for privately created widgets in a group or on site
+	if ((int) $object->access_id === ACCESS_PRIVATE) {
+		$old_ia = elgg_set_ignore_access();
+		if ($owner instanceof ElggGroup) {
+			$object->access_id = $owner->group_acl;
+			$object->save();
+		} elseif ($owner instanceof ElggSite) {
+			$object->access_id = ACCESS_PUBLIC;
+			$object->save();
+		}
+		elgg_set_ignore_access($old_ia);
+	}
+	
+	// Adds a relation between a widget and a multidashboard object
+	$dashboard_guid = get_input('multi_dashboard_guid');
+	if (empty($dashboard_guid) || !widget_manager_multi_dashboard_enabled()) {
+		return;
+	}
+	
+	$dashboard = get_entity($dashboard_guid);
+	if (!elgg_instanceof($dashboard, 'object', MultiDashboard::SUBTYPE, 'MultiDashboard')) {
+		return;
+	}
+	
+	add_entity_relationship($object->getGUID(), MultiDashboard::WIDGET_RELATIONSHIP, $dashboard->getGUID());
 }
 
 /**
