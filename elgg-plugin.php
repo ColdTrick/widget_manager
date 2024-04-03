3e516d250d1e8bf064a0b421dc34269ffc7e3918
Jerôme Bakker <jeabakker@coldtrick.com>
2024-04-03 13:03:14 +0200

    chore: wrapping up v16.1

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -14,7 +14,7 @@ if (is_dir(__DIR__ . '/vendor')) {
 
 return [
 	'plugin' => [
-		'version' => '16.0.2',
+		'version' => '16.1',
 		'dependencies' => [
 			'profile' => [
 				'must_be_active' => false,
